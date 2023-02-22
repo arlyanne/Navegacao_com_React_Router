b5bdb042f7f4736077eef6d96839f00bb6003178
Arlyanne <arlyanne.lima.ferro@gmail.com>
2023-02-22 16:56:32 -0300

    Aula sobre navegação com React Router

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,36 @@
+import './Menu.css'
+import React from "react"
+
+import { Link } from 'react-router-dom'
+
+const Menu = props => (
+    <aside className="Menu">
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Inicio</Link>
+                </li>
+
+                <li>
+                    <Link to="/param/123">Param #01</Link>
+                </li>
+
+                <li>
+                    <Link to="/param/legal">Param #02</Link>
+                </li>
+                
+                <li>
+                    <Link to="/about">Sobre</Link>
+                </li>
+
+                <li>
+                    <Link to="/naoexiste">Não Existe</Link>
+                </li>
+
+                
+            </ul>
+        </nav>
+    </aside>
+)
+
+export default Menu
